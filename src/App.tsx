9e8f24672a783def3be2c68f5f4e1c08462050ba
Raman Aujla <raujla@developrise.com>
2024-09-12 22:07:55 -0400

    WICKED-004 - created routes for home, register, login and forgot-password

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import routerBindings, {
 import { App as AntdApp } from "antd";
 import { createClient } from "graphql-ws";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home, ForgotPassword, Login, Register } from "./pages";
 
 function App() {
   return (
@@ -37,6 +38,10 @@ function App() {
             >
               <Routes>
                 <Route index element={<WelcomePage />} />
+                <Route index element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
               </Routes>
               <RefineKbar />
               <UnsavedChangesNotifier />
